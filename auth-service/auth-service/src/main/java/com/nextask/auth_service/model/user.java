a03d92d16dfbb04733751b34299f0189622e0368
nguyenvuong-dev <nguyenvuong23112004@gmail.com>
2026-06-18 21:24:35 +0700

    feat: initialize auth service

---
+++ auth-service/auth-service/src/main/java/com/nextask/auth_service/model/user.java
@@ -0,0 +1,57 @@
+package com.nextask.auth_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    String name;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false,unique = true,length = 100)
+    String email;
+
+    @Column(nullable = false,unique = true,length = 100)
+    String fullName;
+
+    @Column(nullable = false,unique = true,length = 100)
+    String phone;
+
+    @Column(nullable = false)
+    @Builder.Default
+    Boolean enabled = false;
+
+    @Column(nullable = false)
+    @Builder.Default
+    Boolean disblaed = false;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    LocalDateTime updatedAt;
+
+    @Column(updatable = false)
+    LocalDateTime emailVerifiedAt;
+}
